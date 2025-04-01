ac6153c5c41cc66b24f5f42f0496e50b669c2a7b
DesignHumain <ivandawidowicz@gmail.com>
2025-04-01 09:33:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let bodygraph = document.getElementById("bodygraph");
+    bodygraph.innerHTML = "<p>🔮 Votre Bodygraph apparaîtra ici 🔮</p>";
+});
